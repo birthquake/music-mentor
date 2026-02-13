536946a6aabab6290c806589d3393b2dc57e90ae
birthquake <rashiedtyre@gmail.com>
2026-02-12 23:04:44 -0500

    Update CalendarBooking.js

--- src/CalendarBooking.js
+++ src/CalendarBooking.js
@@ -37,11 +37,6 @@ const ArrowLeftIcon = () => (
   </svg>
 );
 
-const ArrowRightIcon = () => (
-  <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-    <path d="m9 18 6-6-6-6"/>
-  </svg>
-);
 
 /* ============================================
    STEP PROGRESS BAR
@@ -449,6 +444,7 @@ const CalendarBooking = ({ mentor, user, onClose, onConfirm, isOpen }) => {
     if (mentor?.id) {
       loadMentorAvailability();
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [mentor?.id]);
 
   const availableDates = React.useMemo(() => {
